44e7ea4d89f05a5f49a0d9714ac3bfd3a7b78a00
Michał Ossowski <164339462+deGousee1@users.noreply.github.com>
2024-04-05 16:36:00 +0200

    Fixed a typo
    Fixed a typo in "Enter the letter for scrambling..." to "Enter text for scrambling..."

--- src/Main.java
+++ src/Main.java
@@ -35,7 +35,7 @@ public class Main {
         System.out.println("Choose the starting position of the third rotor: ");
         int positionRotor3 = scanner.nextInt() - 1;
 
-        System.out.println("Enter the letter for scrambling (Only uppercase letters): ");
+        System.out.println("Enter the text for scrambling (Only uppercase letters and no spaces): ");
         String textToScramble = scanner.next();
         int textLength = textToScramble.length();
 
