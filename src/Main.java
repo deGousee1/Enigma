91ab326cac80a7ba4e3453a609ced415660da75a
Michal Ossowski <michalossowski12@wp.pl>
2024-04-05 16:01:32 +0200

    changed the way user adds text and converted the ResultList to a String

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -16,17 +15,17 @@ public class Main {
         List<Integer> rotor3Scrambler = Arrays.asList(2, 5, 0, 13, 22, 1, 15, 20, 12, 18, 14, 25, 8, 3, 10, 6, 23, 19, 9, 17, 7, 24, 4, 16, 21, 11);
         // Reverser letter scrambler list
         List<Integer> reverserScrambler = Arrays.asList(11, 3, 13, 1, 5, 4, 16, 12, 22, 23, 17, 0, 7, 2, 24, 18, 6, 10, 15, 21, 25, 19, 8, 9, 14, 20);
-        // Result list
-        List<Character> ResultList = new ArrayList<>(List.of());
 
         // Necessary variables initialization
         int reverserInput = 0;
         // Result variables
         char resultLetter;
+        // Result string
+        StringBuilder result = new StringBuilder();
 
         // Text length
-        System.out.println("Type in the length of the text you want to scramble: ");
-        int textLength = scanner.nextInt();
+        //System.out.println("Type in the length of the text you want to scramble: ");
+        //int textLength = scanner.nextInt();
 
         // Initial setting of rotors made by the user
         System.out.println("Choose the starting position of the first rotor (from 1 to 26): ");
@@ -36,21 +35,24 @@ public class Main {
         System.out.println("Choose the starting position of the third rotor: ");
         int positionRotor3 = scanner.nextInt() - 1;
 
+        System.out.println("Enter the letter for scrambling (Only uppercase letters): ");
+        String textToScramble = scanner.next();
+        int textLength = textToScramble.length();
+
         // Calling the ScramblingEngine constructor
         ScramblingEngine engine = new ScramblingEngine(alphabet, rotor1Scrambler, rotor2Scrambler, rotor3Scrambler, reverserScrambler, reverserInput, positionRotor1, positionRotor2, positionRotor3);
 
         for (int i = 0; i < textLength; i++) {
             // Letter input and rotor position display
             System.out.println("Rotor 1: " + (positionRotor1 + 1) + ", Rotor2: " + (positionRotor2 + 1) + ", Rotor3: " + (positionRotor3 + 1) + ".");
-            System.out.println("Enter the letter for scrambling (Only uppercase letters): ");
-            char letter = scanner.next().charAt(0);
+            char letter = textToScramble.charAt(i);
 
             // Scrambling engine
             // Getting the result from the engine
             resultLetter = engine.scramble(letter);
-            //Printing result letter
+            //Printing result letter and adding resultLetter to the Result string
             System.out.println("Scrambling result: " + resultLetter);
-            ResultList.add(resultLetter);
+            result.append(resultLetter);
 
             // Automatic rotor movement
             // Rotor 3 position calculation order and update
@@ -66,6 +68,6 @@ public class Main {
             engine.updateRotor1Position(positionRotor1);
         }
         // Printing the full result
-        System.out.println("Scrambled text: " + ResultList);
+        System.out.println("Scrambled text: " + result);
     }
 }
